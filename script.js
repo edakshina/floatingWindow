763e85cbc76a8fd75eb7e6d25d78877d5c307b65
EdAksHinA <148536746+edakshina@users.noreply.github.com>
2023-10-20 14:55:38 +0530

    Add files via upload
    This is the related files with the floating window code.  It is made using HTML,CSS and JavaScript in very easy way.

---
+++ script.js
@@ -0,0 +1,53 @@
+// Function to make the window draggable
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "Header")) {
+		
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+// Open the floating window when the button is clicked
+document.getElementById("openBtn").addEventListener("click", function() {
+    document.getElementById("floatingWindow").style.display = "block";
+    dragElement(document.getElementById("floatingWindow"));
+});
+
+// Close the floating window when the close button is clicked
+document.getElementById("closeBtn").addEventListener("click", function() {
+    document.getElementById("floatingWindow").style.display = "none";
+});
